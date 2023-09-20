9519420e3f423bbe84b5e4f3af0da903b30f176f
Joseph Leedy <joseph@leedy.us>
2023-09-20 10:40:01 -0500

    Fix incorrect code indentation in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.scss'
 import Navigation from './components/Navigation'
 
 function App() {
-  // const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
 
   return (
     <>
