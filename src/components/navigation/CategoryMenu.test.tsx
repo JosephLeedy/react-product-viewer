33d3e129b04e362efb0b905417ddf93e795dc8fa
Joseph Leedy <joseph@leedy.us>
2023-10-11 16:30:27 -0500

    Add logic to render Category Menu from JSON data

---
+++ src/components/navigation/CategoryMenu.test.tsx
@@ -0,0 +1,58 @@
+import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
+import {expect, SpyInstance} from 'vitest'
+import CategoryMenu, {Category} from './CategoryMenu'
+import categories from '../../test/data/categories.json'
+
+describe('Category Menu Component', (): void => {
+    let fetchMock: SpyInstance<[input: RequestInfo | URL, init?: RequestInit | undefined], Promise<Response>>
+
+    beforeEach((): void => {
+        fetchMock = vi.spyOn(global, 'fetch').mockImplementation(() => {
+            return Promise.resolve({
+                ok: true,
+                status: 200,
+                json: async (): Promise<Category> => categories,
+            } as Response)
+        })
+    })
+
+    afterEach((): void => {
+        fetchMock.mockRestore()
+    })
+
+    it('renders a loading indicator until categories are fetched', async (): Promise<void> => {
+        render(<CategoryMenu/>)
+
+        const loadingIndicator: HTMLElement = screen.getByTestId('categories-loading-indicator')
+
+        expect(loadingIndicator).toBeInTheDocument()
+        expect(screen.getByText('Loading categories...')).toBeInTheDocument()
+
+        await waitForElementToBeRemoved(() => screen.getByTestId('categories-loading-indicator'))
+    })
+
+    it('renders fetched categories as menu items', async (): Promise<void> => {
+        render(<CategoryMenu/>)
+
+        const categoryMenu: HTMLElement = await screen.findByTestId('category-menu')
+
+        expect(categoryMenu).toBeInTheDocument()
+        expect(categoryMenu.children).toHaveLength(categories.children_data.length)
+
+        const categoryNames: string[] = []
+        const getCategoryNames = (category: Category): void => {
+            categoryNames.push(category.name)
+
+            if (category.children_data.length > 0) {
+                category.children_data.forEach(getCategoryNames)
+            }
+        }
+        let categoryName: string
+
+        categories.children_data.forEach(getCategoryNames)
+
+        for (categoryName of categoryNames) {
+            expect(screen.getByText(categoryName)).toBeInTheDocument()
+        }
+   })
+})
--- src/components/navigation/CategoryMenu.tsx
+++ src/components/navigation/CategoryMenu.tsx
@@ -1,34 +1,103 @@
-import Nav from 'react-bootstrap/Nav'
-import Dropdown from 'react-bootstrap/Dropdown'
+import React, {useEffect, useState} from 'react'
+import Spinner from 'react-bootstrap/Spinner'
+import {convertTitleToUri} from '../../utilites/UriConverter'
 
-export default () => {
+export interface Category {
+    id: number;
+    parent_id: number;
+    name: string;
+    is_active: boolean;
+    position: number;
+    level: number;
+    product_count: number;
+    children_data: Category[];
+}
+
+const defaultCategory: Category = {
+    id: 0,
+    parent_id: 0,
+    name: '',
+    is_active: true,
+    position: 0,
+    level: 1,
+    product_count: 0,
+    children_data: []
+};
+
+export default function CategoryMenu(): React.JSX.Element {
+    const [isLoadingCategories, setIsLoadingCategories] = useState<boolean>(false)
+    const [rootCategory, setRootCategory] = useState<Category>(defaultCategory)
+    const loadCategories = async (): Promise<void> => {
+        setIsLoadingCategories(true)
+
+        const response: Response = await fetch('/data/categories.json')
+
+        setIsLoadingCategories(false)
+
+        if (!response.ok) {
+            throw new Error(`Could not load categories. Response: ${response.status} ${response.statusText}`);
+        }
+
+        setRootCategory(await response.json())
+    }
+    const renderCategoryMenuItems = (
+        category: Category,
+        isDropdownItem: boolean = false,
+        uriSegments: string[] = []
+    ) => {
+        const hasChildren: boolean = category.children_data.length > 0
+
+        uriSegments.push(convertTitleToUri(category.name))
+
+        return (
+            <li key={category.id} className={`nav-item` + (hasChildren ? ' dropdown' : '')}>
+                <a href={`#${uriSegments.join('/')}`}
+                   className={
+                       `nav-link` + (hasChildren ? ' dropdown-toggle' : '') + (isDropdownItem ? ' dropdown-item' : '')
+                   }
+                   {...(hasChildren && {
+                       "role": "button",
+                       "data-bs-toggle": "dropdown",
+                       "aria-expanded": false
+                   })}
+                >{category.name}</a>
+                {hasChildren &&
+                    <ul className="dropdown-menu">
+                        {
+                            category.children_data.map((childCategory: Category) => {
+                                const categorySubMenu: React.JSX.Element = renderCategoryMenuItems(
+                                    childCategory,
+                                    true,
+                                    uriSegments
+                                )
+
+                                uriSegments.splice(-1)
+
+                                return categorySubMenu
+                            })
+                        }
+                    </ul>
+                }
+            </li>
+        )
+    }
+
+    useEffect((): void => {
+        loadCategories().catch(console.log)
+    }, [])
+
     return (
-        <Nav as="ul" className="me-auto">
-            <Nav.Item as="li">
-                <Nav.Link href="#category-0">Category 0</Nav.Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-                <Dropdown id="category-1-dropdown">
-                    <Dropdown.Toggle as={Nav.Link}>Category 1</Dropdown.Toggle>
-                    <Dropdown.Menu as="ul" renderOnMount={true}>
-                        <li><Dropdown.Item href="#category-1.1">Category 1.1</Dropdown.Item></li>
-                    </Dropdown.Menu>
-                </Dropdown>
-            </Nav.Item>
-            <Nav.Item as="li">
-                <Nav.Link href="#category-2">Category 2</Nav.Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-                <Nav.Link href="#category-3">Category 3</Nav.Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-                <Dropdown id="category-4-dropdown">
-                    <Dropdown.Toggle as={Nav.Link}>Category 4</Dropdown.Toggle>
-                    <Dropdown.Menu as="ul" renderOnMount={true}>
-                        <li><Dropdown.Item href="#category-4.1">Category 4.1</Dropdown.Item></li>
-                    </Dropdown.Menu>
-                </Dropdown>
-            </Nav.Item>
-        </Nav>
+        <>
+            {
+                isLoadingCategories ?
+                    <Spinner animation="border" role="status" data-testid="categories-loading-indicator">
+                        <span className="visually-hidden">Loading categories...</span>
+                    </Spinner>
+                    :
+                    <ul className="navbar-nav me-auto" data-testid="category-menu">
+                        {rootCategory.children_data.map((category: Category) => renderCategoryMenuItems(category))}
+                    </ul>
+            }
+        </>
     )
 }
