377ba079edba56f0378fa39bb4771e88397e8ba5
Joseph Leedy <joseph@leedy.us>
2023-10-26 15:04:51 -0500

    Remove example state variable from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-// import { useState } from 'react'
 import './App.scss'
 import Navigation from './components/Navigation'
 import ProductGrid from './components/ProductGrid'
 import Footer from './components/Footer.tsx'
 
 function App() {
-    // const [count, setCount] = useState(0)
-
     return (
         <>
             <Navigation/>
