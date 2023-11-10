ab9b7596ad1c475042ed65551413f5af927acbab
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2023-11-10 10:30:28 -0600

    Fix broken test for navigation menu component

--- src/components/Navigation.test.tsx
+++ src/components/Navigation.test.tsx
@@ -1,22 +1,36 @@
 import {render, screen} from '@testing-library/react'
+import {CategoriesContextProvider} from '../contexts/CategoriesContext'
 import Navigation from './Navigation'
+import rootCategory from '../test/data/categories.json'
 
 describe('Navigation bar renders properly', () => {
     test('it has the app name as a main heading', () => {
-        const { getByText } =  render(<Navigation/>)
+        const { getByText } =  render(
+            <CategoriesContextProvider isLoadingCategories={false} categories={rootCategory.children_data}>
+                <Navigation/>
+            </CategoriesContextProvider>
+        )
 
         expect(getByText('Product Viewer')).toBeInTheDocument()
         expect(screen.getByRole('heading', {level: 1})).toBeInTheDocument()
     })
 
     test('it has a navigation menu', () => {
-        render(<Navigation/>)
+        render(
+            <CategoriesContextProvider isLoadingCategories={false} categories={rootCategory.children_data}>
+                <Navigation/>
+            </CategoriesContextProvider>
+        )
 
         expect(screen.getByRole('navigation')).toBeInTheDocument()
     })
 
     test('it has a search form', () => {
-        render(<Navigation/>)
+        render(
+            <CategoriesContextProvider isLoadingCategories={false} categories={rootCategory.children_data}>
+                <Navigation/>
+            </CategoriesContextProvider>
+        )
 
         expect(screen.getByRole('search')).toBeInTheDocument()
     })
