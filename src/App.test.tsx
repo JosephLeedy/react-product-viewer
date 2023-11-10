f1231e353edd1a21851c7d3b3b041ce173a8a90e
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2023-11-10 10:37:26 -0600

    Fix broken test for application component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,30 @@
+import {Mock} from 'vitest'
 import {getByTestId, render} from '@testing-library/react'
 import App from './App'
+import useCategories from './hooks/useCategories'
+import Category from './types/Category'
+import rootCategory from './test/data/categories.json'
 
 describe("App renders properly", () => {
+    const useCategoriesMock: Mock = vi.hoisted((): Mock => vi.fn())
+
+    vi.mock('./hooks/useCategories', (): { default: Mock } => ({
+        default: useCategoriesMock
+    }))
+
+    beforeEach((): void => {
+        const useCategoriesReturnValue: {isLoadingCategories: boolean, categories: Category[]} = {
+            isLoadingCategories: false,
+            categories: rootCategory.children_data
+        }
+
+        vi.mocked(useCategories).mockReturnValue(useCategoriesReturnValue)
+    });
+
+    afterEach((): void => {
+        vi.resetAllMocks()
+    })
+
     test("it has a header", () => {
         render(<App/>)
 
