830db146d272be74c97de5c3c696b0242020481c
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2023-11-13 16:24:17 -0600

    Create configuration variables for product URLs

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_MAGENTO_BASE_URL: string
+    readonly VITE_MAGENTO_IMAGE_URL: string
+    readonly VITE_MAGENTO_PLACEHOLDER_IMAGE_URL: string
+    readonly VITE_MAGENTO_URI_SUFFIX: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
