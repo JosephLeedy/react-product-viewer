93f50f2688689b1405b12d526adb5af4d23e6e9f
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2023-11-15 16:21:15 -0600

    Create helper functions for filtering product data

---
+++ src/helpers/productFilter.test.ts
@@ -0,0 +1,130 @@
+import {
+    filterComplexProducts,
+    filterEnabledProducts,
+    filterProductsByCategoryId,
+    filterProductsByName,
+    filterProductsBySku,
+    filterUncomplexProducts
+} from './productFilter'
+import Product, {ProductStatus, ProductTypeId} from '../types/Product'
+import productData from '../test/data/products.json'
+
+// @ts-ignore
+const products: Product[] = productData.items
+
+describe('Product Filtering Helpers', (): void => {
+    describe('Filter Enabled Products', (): void => {
+        it('returns only enabled products', (): void => {
+            const enabledProducts: Product[] = filterEnabledProducts(products)
+
+            enabledProducts.forEach((product: Product): void => {
+                expect(product).toEqual(
+                    expect.objectContaining({
+                        status: ProductStatus.Enabled
+                    })
+                )
+            })
+        })
+    })
+
+    describe('Filter Uncomplex Products', (): void => {
+        it('returns only simple, virtual, downloadable and gift card products', (): void => {
+            const uncomplexProducts: Product[] = filterUncomplexProducts(products)
+            const uncomplexProductTypes: ProductTypeId[] = [
+                ProductTypeId.Simple,
+                ProductTypeId.Virtual,
+                ProductTypeId.Downloadable,
+                ProductTypeId.GiftCard
+            ]
+
+            uncomplexProducts.forEach((product: Product): void => {
+                expect(product.type_id).toBeOneOf(uncomplexProductTypes)
+            })
+        })
+    })
+
+    describe('Filter Complex Products', (): void => {
+        it('returns only configurable, grouped and bundle products', (): void => {
+            const complexProducts: Product[] = filterComplexProducts(products)
+            const complexProductTypes: ProductTypeId[] = [
+                ProductTypeId.Configurable,
+                ProductTypeId.Grouped,
+                ProductTypeId.Bundle
+            ]
+
+            complexProducts.forEach((product: Product): void => {
+                expect(product.type_id).toBeOneOf(complexProductTypes)
+            })
+        })
+    })
+
+    describe('Filter Products by Category ID', (): void => {
+        it('returns products within the given category', (): void => {
+            const categoryProducts: Product[] = filterProductsByCategoryId(products, 5)
+
+            categoryProducts.forEach((product: Product): void => {
+                expect(product.extension_attributes.category_links).toEqual(
+                    expect.arrayContaining([
+                        expect.objectContaining({
+                            category_id: "5"
+                        })
+                    ])
+                )
+            })
+        })
+    })
+
+    describe('Filter Products by Name', (): void => {
+        it.each([
+            {
+                matchType: 'fully',
+                name: 'Luma Virtual Gift Card',
+                matchingIds: [2042]
+            },
+            {
+                matchType: 'partially',
+                name: 'Yoga',
+                matchingIds: [
+                    21,
+                    33,
+                    34,
+                    35,
+                    45,
+                    46,
+                    2044
+                ]
+            },
+        ])('returns products $matchType matching name "$name"', ({name, matchingIds}): void => {
+            const expectedProducts: Product[] = products.filter((product: Product) => matchingIds.includes(product.id))
+            const matchedProducts: Product[] = filterProductsByName(products, name)
+
+            expect(matchedProducts).not.toBeEmpty()
+            expect(matchedProducts).toEqual(expectedProducts)
+        })
+    })
+
+    describe('Filter Products by SKU', (): void => {
+        it.each([
+            {
+                matchType: 'fully',
+                sku: '243-MB09',
+                matchingIds: [2042]
+            },
+            {
+                matchType: 'partially',
+                sku: '24-WG083',
+                matchingIds: [
+                    30,
+                    31,
+                    32
+                ]
+            },
+        ])('returns products $matchType matching SKU "$sku"', ({sku, matchingIds}): void => {
+            const expectedProducts: Product[] = products.filter((product: Product) => matchingIds.includes(product.id))
+            const matchedProducts: Product[] = filterProductsBySku(products, sku)
+
+            expect(matchedProducts).not.toBeEmpty()
+            expect(matchedProducts).toEqual(expectedProducts)
+        })
+    })
+})
---
+++ src/helpers/productFilter.ts
@@ -0,0 +1,52 @@
+import Product, {CategoryLink, ProductStatus, ProductTypeId} from '../types/Product'
+
+export function filterEnabledProducts(productsToFilter: Product[]): Product[] {
+    return productsToFilter.filter((product: Product): boolean => product.status === ProductStatus.Enabled)
+}
+
+export function filterUncomplexProducts(productsToFilter: Product[]): Product[] {
+    const uncomplexProductTypes: ProductTypeId[] = [
+        ProductTypeId.Simple,
+        ProductTypeId.Virtual,
+        ProductTypeId.Downloadable,
+        ProductTypeId.GiftCard
+    ]
+
+    return productsToFilter.filter((product: Product): boolean => uncomplexProductTypes.includes(product.type_id))
+}
+
+export function filterComplexProducts(productsToFilter: Product[]): Product[] {
+    const complexProductTypes: ProductTypeId[] = [
+        ProductTypeId.Configurable,
+        ProductTypeId.Grouped,
+        ProductTypeId.Bundle
+    ]
+
+    return productsToFilter.filter((product: Product): boolean => complexProductTypes.includes(product.type_id))
+}
+
+export function filterProductsByCategoryId(productsToFilter: Product[], categoryId: number): Product[] {
+    return productsToFilter.filter((product: Product): boolean => {
+        let categoryLink: CategoryLink | undefined
+
+        if (!product.extension_attributes.category_links?.length) {
+            return false
+        }
+
+        categoryLink = product.extension_attributes.category_links.find(
+            (categoryLink: CategoryLink): boolean => parseInt(categoryLink.category_id, 10) === categoryId
+        )
+
+        return categoryLink !== undefined;
+    })
+}
+
+export function filterProductsByName(productsToFilter: Product[], name: string): Product[] {
+    return productsToFilter.filter(
+        (product: Product): boolean => product.name.toLowerCase().includes(name.toLowerCase())
+    )
+}
+
+export function filterProductsBySku(productsToFilter: Product[], sku: string): Product[] {
+    return productsToFilter.filter((product: Product): boolean => product.sku.toLowerCase().includes(sku.toLowerCase()))
+}
