0ab7e7a4d5050beedb1ff980b5c2c48d9e0bc255
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2023-11-30 14:24:33 -0600

    Move search form to separate component

---
+++ src/components/Navigation/SearchForm.test.tsx
@@ -0,0 +1,8 @@
+import {render} from '@testing-library/react'
+import SearchForm from './SearchForm'
+
+test('Search Form Component', (): void => {
+    it('', (): void => {
+        render(<SearchForm/>)
+    })
+})
---
+++ src/components/Navigation/SearchForm.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form'
+import InputGroup from 'react-bootstrap/InputGroup'
+import {Search} from 'react-bootstrap-icons'
+
+export default function SearchForm(): React.JSX.Element {
+    return (
+        <Form role="search">
+            <InputGroup>
+                <InputGroup.Text>
+                    <Search/>
+                </InputGroup.Text>
+                <Form.Control type="search" placeholder="Search"/>
+            </InputGroup>
+        </Form>
+    )
+}
