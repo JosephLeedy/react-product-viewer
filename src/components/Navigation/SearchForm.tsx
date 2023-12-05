b2d2b73793d1385cb32b3c49355513ab7a2abc8f
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2023-12-05 11:26:26 -0600

    Prevent search form from being submitted

--- src/components/Navigation/SearchForm.tsx
+++ src/components/Navigation/SearchForm.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, {FormEvent} from 'react';
 import Form from 'react-bootstrap/Form'
 import InputGroup from 'react-bootstrap/InputGroup'
 import {Search} from 'react-bootstrap-icons'
 
 export default function SearchForm(): React.JSX.Element {
     return (
-        <Form role="search">
+        <Form role="search" onSubmit={(event: FormEvent): void => {event.preventDefault()}}>
             <InputGroup>
                 <InputGroup.Text>
                     <Search aria-hidden/>
