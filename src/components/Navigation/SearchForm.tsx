5c06c3953ede750c56095fa6bc7028b695cac619
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2023-12-05 13:28:23 -0600

    Use context to set value for search query input

--- src/components/Navigation/SearchForm.tsx
+++ src/components/Navigation/SearchForm.tsx
@@ -1,16 +1,19 @@
 import React, {FormEvent} from 'react';
+import {useSearchQueryContext} from '../../contexts/SearchQueryContext'
 import Form from 'react-bootstrap/Form'
 import InputGroup from 'react-bootstrap/InputGroup'
 import {Search} from 'react-bootstrap-icons'
 
 export default function SearchForm(): React.JSX.Element {
+    const {searchQuery} = useSearchQueryContext()
+
     return (
         <Form role="search" onSubmit={(event: FormEvent): void => {event.preventDefault()}}>
             <InputGroup>
                 <InputGroup.Text>
                     <Search aria-hidden/>
                 </InputGroup.Text>
-                <Form.Control type="search" placeholder="Search catalog"/>
+                <Form.Control type="search" value={searchQuery} placeholder="Search catalog"/>
             </InputGroup>
         </Form>
     )
