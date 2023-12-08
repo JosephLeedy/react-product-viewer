d9d61c1eff798d738dfe59ad48df62920e2e1a34
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2023-12-08 15:57:40 -0600

    Add missing return types to functions in URI Converter

--- src/utilities/UriConverter.ts
+++ src/utilities/UriConverter.ts
@@ -1,9 +1,9 @@
-export const convertTitleToUri = (title: string) => title
+export const convertTitleToUri = (title: string): string => title
     .trim()
     .toLowerCase()
     .replace(/[^a-z0-9-]+/g, '-')
     .replace(/-$/, '')
 
-export const convertUriToTitle = (uri: string) => uri.replace(/-s-/g, "'s-")
+export const convertUriToTitle = (uri: string): string => uri.replace(/-s-/g, "'s-")
     .replace(/-+/g, ' ')
-    .replace(/(?<= )\S|^./g, firstLetter => firstLetter.toUpperCase())
+    .replace(/(?<= )\S|^./g, (firstLetter: string) => firstLetter.toUpperCase())
