8c42ae8d60b4bb5524d9c9b82a8bb4bb36922c45
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2023-12-08 14:32:39 -0600

    Revert "Suppress error in ReactBuddy hook"
    This reverts commit 35c270b8695d0dacbdff36372ce7294171f7703a.

--- src/dev/useInitial.ts
+++ src/dev/useInitial.ts
@@ -2,7 +2,6 @@ import {useState} from 'react'
 import {InitialHookStatus} from '@react-buddy/ide-toolbox'
 
 export const useInitial: () => InitialHookStatus = () => {
-    // @ts-ignore
     const [status, setStatus] = useState<InitialHookStatus>({
         loading: false,
         error: false,
