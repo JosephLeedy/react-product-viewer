cfbaa98406d4d1e19c938a92a245f8ae6e5cf920
Joseph Leedy <3091468+JosephLeedy@users.noreply.github.com>
2024-02-01 10:40:33 -0600

    Improve error handling for categories API requests

--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -4,25 +4,40 @@ import type Category from '../types/Category'
 type UseCategories = {
     isLoadingCategories: boolean
     categories: Category[]
+    errorMessage: string
 }
 
 export default function useCategories(): UseCategories {
     const [isLoadingCategories, setIsLoadingCategories] = useState<boolean>(true)
     const [categories, setCategories] = useState<Category[]>([])
+    const [errorMessage, setErrorMessage] = useState<string>('')
     const loadCategories = async (): Promise<void> => {
         await fetch(`${import.meta.env.VITE_BACKEND_URL}/catalog/categories`)
             .then(async (response: Response): Promise<void> => {
                 let rootCategory: Category
+                let responseMessage: string | null = null
 
                 if (!response.ok) {
-                    throw new Error(`Could not load categories. Response: ${response.status} ${response.statusText}`)
+                    responseMessage = (await response.json()).message ?? null
+
+                    if (responseMessage !== null) {
+                        setErrorMessage(`Could not load categories. Error: "${responseMessage}"`)
+                    } else {
+                        setErrorMessage(
+                            `Could not load categories. Response: ${response.status} ${response.statusText}`
+                        )
+                    }
+
+                    setIsLoadingCategories(false)
+
+                    return
                 }
 
                 rootCategory = await response.json() as Category
 
                 setCategories(rootCategory.children_data)
             }).catch((error: Error): void => {
-                console.log(error)
+                setErrorMessage(`Could not load categories. Error: "${error.message}"`)
             }).finally((): void => {
                 setIsLoadingCategories(false)
             })
@@ -34,6 +49,7 @@ export default function useCategories(): UseCategories {
 
     return {
         isLoadingCategories,
-        categories
+        categories,
+        errorMessage
     }
 }
